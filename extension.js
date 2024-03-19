7548e61d755ee77fd9e2880de687ec48e737b604
Tushar Jain <70627686+hiTushar@users.noreply.github.com>
2024-03-19 23:14:41 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,13 +10,13 @@ const vscode = require("vscode");
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "google-it" is now active!');
+  // console.log('Congratulations, your extension "google-it" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "google-it.helloWorld",
+    "google-this.googleThis",
     function () {
       // The code you place here will be executed every time your command is executed
 
